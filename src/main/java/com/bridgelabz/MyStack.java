3060d3813f708a9328358ead565266dd671960d2
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-10 18:31:23 +0530

    ADDED:To stack method push linked list

--- src/main/java/com/bridgelabz/MyStack.java
+++ src/main/java/com/bridgelabz/MyStack.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
 public class MyStack {
-    public static void main(String[] args) {
-        System.out.println("Welcome stack and queue data structure project");
+    private final MyLinkList linkList;
+
+    public MyStack() {
+        this.linkList = new MyLinkList();
+    }
+
+    public void push(MyNode myNode) {
+        linkList.addAtStart(myNode);
+    }
+
+    public void printStack() {
+        linkList.printMyNode();
+    }
+
+    public MyNode peak() {
+        return linkList.start;
     }
 }
--- src/test/java/com/bridgelabz/MyStackTest.java
+++ src/test/java/com/bridgelabz/MyStackTest.java
@@ -1,7 +1,42 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MyStackTest {
 
+    @Test
+    public void Provide3Number_WhenAddedToStackInSequence_TestCaseShouldPass(){
+        MyStack myStack = new MyStack();
+        MyNode<Integer> firstNode = new MyNode<>(70);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> threadNode = new MyNode<>(56);
+
+        myStack.push(firstNode);
+        myStack.push(secondNode);
+        myStack.push(threadNode);
+
+        MyNode peak = myStack.peak();
+        myStack.printStack();
+
+        Assert.assertEquals(threadNode, peak );
+    }
+
+    @Test
+    public void Provide3Number_WhenAddedToStackNotInSequence_TestCaseShouldPass(){
+        MyStack myStack = new MyStack();
+        MyNode<Integer> firstNode = new MyNode<>(70);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> threadNode = new MyNode<>(56);
+
+        myStack.push(firstNode);
+        myStack.push(secondNode);
+        myStack.push(threadNode);
+
+        MyNode peak = myStack.peak();
+        myStack.printStack();
+
+        Assert.assertNotEquals(firstNode, peak );
+    }
 }
 
