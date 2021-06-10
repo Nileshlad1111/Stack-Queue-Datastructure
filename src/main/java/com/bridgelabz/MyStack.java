6ce0cdec253732571156cf9f54c1b12ecc84d91e
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-10 18:16:01 +0530

    ADDED:welcome message

---
+++ src/main/java/com/bridgelabz/MyStack.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class MyStack {
+    public static void main(String[] args) {
+        System.out.println("Welcome stack and queue data structure project");
+    }
+}
